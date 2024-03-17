3852f003357918b27b1b59cd4b7400e5365a18b2
biohotR <silvestru.radu2002@gmail.com>
2024-03-17 17:47:21 +0200

    added a note taking file

---
+++ notes.md
@@ -0,0 +1,15 @@
+# Includes stuff that I feel are important
+
+## higher order functions in racket
+
+### map
+***map*** is higher-order function that applies a given function to each element of one or more lists
+collecting the result in a new list that it returns
+
+Basic overview:
+``(map function list1 list 2)``
+
+Simple example:
+``(map + '(1 2 3) '(4 5 6))``
+
+
--- test.rkt
+++ test.rkt
@@ -1,37 +1,8 @@
 #lang racket
-(define (compare imga imgb)
-    (or (> (image-height imga) (image-height imgb))
-        (and (= (image-height imga) (image-height imgb))
-            (> (image-width imga) (image-width imgb))))
-)
 
-;; the split function splits the list into two halves by appending to the left list every other element
-
-(define (split lst)
-    (let loop ((left '())
-                (right lst)
-                switch #t))
-    (if (empty? right)
-        (values (reverse left) right)
-        (if switch
-            (loop (cons (car right) left)
-                (cdr right)
-                (not switch))
-            (loop left
-                (cdr right)
-                (not switch))
-        )
-    )
-)
-
-
-(define (mergesort lst)
-  (cond
-    [(empty? lst) lst]
-    [(empty? (cdr lst)) lst]
-    [else
-     (let-values (((left right) (split-list lst)))
-       (merge (mergesort left) (mergesort right)))]))
-
-
-
+(define (to-numbers M)
+  (if (null? M)
+      '()
+      (cons (to-number (car M)) (to-numbers (cdr M)))))
+ 
+(to-numbers matrix) ; '(123 456 789)
