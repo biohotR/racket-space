ec4287eb65aa98918d702efa04dbdd7801b37564
biohotR <silvestru.radu2002@gmail.com>
2024-03-17 16:54:02 +0200

    slight changes

--- test.rkt
+++ test.rkt
@@ -1,16 +1,37 @@
 #lang racket
-
-(define (remove-duplicates-left L)
-    (define (helper L acc)
-        (if (null? L)
-            (reverse acc)
-            
-        (if (member (car L) acc)
-            (helper (cdr L) acc)
-            (helper (cdr L) (cons (car L) acc))))
-    )
-    (helper L '())
+(define (compare imga imgb)
+    (or (> (image-height imga) (image-height imgb))
+        (and (= (image-height imga) (image-height imgb))
+            (> (image-width imga) (image-width imgb))))
 )
 
-(define (remove-duplicates-right L)
+;; the split function splits the list into two halves by appending to the left list every other element
+
+(define (split lst)
+    (let loop ((left '())
+                (right lst)
+                switch #t))
+    (if (empty? right)
+        (values (reverse left) right)
+        (if switch
+            (loop (cons (car right) left)
+                (cdr right)
+                (not switch))
+            (loop left
+                (cdr right)
+                (not switch))
+        )
     )
+)
+
+
+(define (mergesort lst)
+  (cond
+    [(empty? lst) lst]
+    [(empty? (cdr lst)) lst]
+    [else
+     (let-values (((left right) (split-list lst)))
+       (merge (mergesort left) (mergesort right)))]))
+
+
+
